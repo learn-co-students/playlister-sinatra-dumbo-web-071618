893a69633e6432d5f66b9d5ac02225259f3a88c2
= <=>
2018-08-10 10:21:38 -0400

    Adds new songs form

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,6 +4,10 @@ class ArtistsController < ApplicationController
     erb :'artists/index'
   end
 
+  get '/artists/new' do
+    erb :'artists/new'
+  end
+
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,11 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
